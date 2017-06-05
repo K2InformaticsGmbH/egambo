1aa85542935c2cd4a8b7e5bbc666e9d8c83d1415
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2017-06-05 22:41:17 +0200

    adding egambo_tictac_ann bot raw structure

---
+++ src/ann.erl
@@ -0,0 +1,334 @@
+%  ann.erl
+-module(ann).
+-compile([export_all]).
+
+sigmoid(X) -> 1.0 / (1.0 + math:exp(-X)).
+
+activation(X) -> sigmoid(X).
+
+activation_gradient(X) -> X * (1.0 - X).
+
+output_activation(X) -> X.
+
+output_activation_gradient(_) -> 1.0.
+
+run_neuron() ->
+  spawn_link(ann, run_neuron, [none, [], [], 0]).
+
+run_neuron(Main, Inputs, Outputs, Token) ->
+  receive
+    {main_pid, Pid} ->
+      run_neuron(Pid, Inputs, Outputs, Token);
+
+    {connect_to_output, Pid} ->
+      run_neuron(Main, Inputs, [Pid | Outputs], Token);
+
+    {connect_to_input, PidWeight} ->
+      run_neuron(Main, [PidWeight | Inputs], Outputs, Token);
+
+    {status, Token} ->
+      status_neuron(Inputs, Outputs, Token),
+      run_neuron(Main, Inputs, Outputs, Token + 1);
+
+    {feed_forward, Token} ->
+      fire_neuron(Inputs, Outputs, {feed_forward, Token}, Main, Token),
+      run_neuron(Main, Inputs, Outputs, Token + 1);
+
+    {back_prop, LearningRate, Token} ->
+      NewInputs = learn_neuron(Inputs, Outputs, LearningRate, Main, Token),
+      run_neuron(Main, NewInputs, Outputs, Token + 1)
+  end.
+
+%% -----
+% operations on neuron
+%% -----
+status_neuron(Inputs, Outputs, Token) ->
+  io:format("~p -> ~p --> ~p ~n", [self(), Inputs, Outputs]),
+  resend_status_neuron(Inputs, Outputs, Token).
+
+resend_status_neuron([], Outputs, Token) ->
+  lists:foreach(fun(Pid) ->
+                  Pid ! {status, Token}
+                end, Outputs);
+resend_status_neuron(_, _, _) -> ok.
+
+
+% -- learning
+learn_neuron([], Outputs, LearningRate, Main, Token) ->
+  % -- bias neuron
+  fire_neuron([], Outputs, {back_prop, LearningRate, Token}, Main, Token),
+  [receive {delta, Pid, _, Token} -> ignore end || Pid <- Outputs],
+  [];
+learn_neuron(Inputs, [], LearningRate, Main, Token) ->
+  % -- output neuron
+  {Output, Outs} = fire_neuron(Inputs, [], output_layer_fire, Main, Token),
+  receive {target, Target, Token} -> Target end,
+  Delta = (Output - Target) * output_activation_gradient(Output),
+  update_weights(Inputs, Outs, Delta, LearningRate, Token);
+learn_neuron([{Pid, Weight}], Outputs, _, Main, Token) when Pid == Main ->
+  % -- input layer neuron
+  fire_neuron([{Pid, Weight}], Outputs, input_layer_fire, Main, Token),
+  [receive {delta, N, InDelta, Token} -> InDelta end || N <- Outputs],
+  [{Pid, Weight}];
+learn_neuron(Inputs, Outputs, LearningRate, Main, Token) ->
+  % -- hidden layer neuron
+  {Output, Outs} = fire_neuron(Inputs, Outputs, hidden_layer_fire, Main, Token),
+  Delta = lists:sum([receive {delta, Pid, InDelta, Token} -> InDelta end || Pid <- Outputs]) * activation_gradient(Output),
+  update_weights(Inputs, Outs, Delta, LearningRate, Token).
+
+update_weights(Inputs, Outs, Delta, LearningRate, Token) ->
+  Self = self(),
+  lists:map(fun({{Pid, Weight}, Out}) ->
+              Pid ! {delta, Self, Delta * Weight, Token},
+              {Pid, Weight - LearningRate * Delta * Out}
+            end, lists:zip(Inputs, Outs)).
+
+% -- predicting
+fire_neuron([], Outputs, Opt, _, Token) ->
+  % -- bias neuron
+  Self = self(),
+  lists:foreach(fun(Pid) ->
+                  Pid ! Opt, 
+                  Pid ! {fire, Self, 1.0, Token} 
+                end, Outputs),
+  {1.0, []};
+fire_neuron(Inputs, [], _, Main, Token) ->
+  % -- output neuron
+  Self = self(),
+  Input = [receive {fire, Pid, In, Token} -> {Weight * In, In} end || {Pid, Weight} <- Inputs],
+  Output = output_activation(sum_tuples(Input)),
+  Main ! {output, Self, Output, Token},
+  {Output, snd(Input)};
+fire_neuron([{Pid, _}], Outputs, _, Main, Token) when Pid == Main ->
+  % -- input layer
+  Self = self(),
+  Output = receive {fire, Pid, In, Token} -> In end,
+  lists:foreach(fun(N) ->
+                  N ! {fire, Self, Output, Token}
+                end, Outputs),
+  {Output, [Output]};
+fire_neuron(Inputs, Outputs, _, _, Token) ->
+  % -- hidden layer neuron
+  Self = self(),
+  Input = [receive {fire, Pid, In, Token} -> {Weight * In, In} end || {Pid, Weight} <- Inputs],
+  Output = activation(sum_tuples(Input)),
+  lists:foreach(fun(Pid) ->
+                  Pid ! {fire, Self, Output, Token}
+                end, Outputs),
+  {Output, snd(Input)}.
+
+create_neural_network(Layers) ->
+  Weights = random_weigths(compute_neurons(Layers)),
+  create_neural_network(Layers, Weights).
+
+create_neural_network(Layers, Weights) when length(Layers) > 1 ->
+  Neurons = [[run_neuron() || _ <- lists:seq(1, InLayer)] || InLayer <- modify_layers(Layers)],
+  full_mesh_connect(Neurons, Weights),
+  NN = spawn_link(ann, neural_network, [tl(hd(Neurons)), 
+                                        lists:last(Neurons), 
+                                        lists:map(fun(X) -> hd(X) end, 
+                                        lists:droplast(Neurons)), 
+                                        0]),
+  lists:foreach(fun(Pid) -> 
+                  Pid ! {connect_to_input, {NN, 1.0}} 
+                end, tl(hd(Neurons))),
+  lists:foreach(fun(Pid) ->
+                  Pid ! {main_pid, NN}
+                end, lists:concat(Neurons)),
+  NN;
+create_neural_network(_, _) ->
+  io:format("Not enought layers.~n"), fail.
+
+neural_network(InputLayer, OutputLayer, BiasNeurons, Token) ->
+  erlang:garbage_collect(),
+  receive
+    {predict, Input} ->
+      Output = forward_pass(InputLayer, OutputLayer, BiasNeurons, Token, Input),
+      io:format("Output: ~p~n", [Output]),
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token + 1);
+    {predict, Input, OutputPid} ->
+      Output = forward_pass(InputLayer, OutputLayer, BiasNeurons, Token, Input),
+      OutputPid ! {predicted, Output},
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token + 1);
+    {compute_error, TrainingSet} ->
+      Examples = length(TrainingSet),
+      Rss =  forward_pass_examples(InputLayer, OutputLayer, BiasNeurons, Token, TrainingSet, Examples),
+      io:format("RSS: ~p~n", [Rss]),
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token + Examples);
+    {learn_epochs, Epochs, LearningRate, TrainingSet} ->
+      TokenShift = learn_epochs(InputLayer, OutputLayer, BiasNeurons, Token, Epochs, LearningRate, TrainingSet),
+      io:format("Done.~n"),
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token + TokenShift);
+    {learn_until, RssEps, LearningRate, TrainingSet} ->
+      TokenShift = learn_until(InputLayer, OutputLayer, BiasNeurons, Token, RssEps, LearningRate, TrainingSet),
+      io:format("Done.~n"),
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token + TokenShift);
+    {status} ->
+      lists:foreach(fun(N) -> 
+                      N ! {status, Token}
+                    end, BiasNeurons),
+      lists:foreach(fun(N) -> 
+                      N ! {status, Token}
+                    end, InputLayer),
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token + 1);
+    {finish} ->
+      exit(neural_network_shutdown);
+    _ ->
+      neural_network(InputLayer, OutputLayer, BiasNeurons, Token)
+  end.
+
+predict(NN, Set) ->
+  Self = self(),
+  lists:map(fun(Ex) ->
+              NN ! {predict, Ex, Self},
+              receive {predicted, Val} -> Val end,
+              Val
+            end, Set).
+
+learn_epochs(InputLayer, OutputLayer, BiasNeurons, Token, Epochs, LearningRate, TrainingSet) ->
+  Examples = length(TrainingSet),
+  lists:foreach(fun(Epoch) ->
+      learn_pass(InputLayer, OutputLayer, BiasNeurons, Token + Examples * Epoch * 2, LearningRate, TrainingSet, Examples),
+      Rss = forward_pass_examples(InputLayer, OutputLayer, BiasNeurons, Token + Examples * (Epoch * 2 + 1), TrainingSet, Examples),
+      io:format("Epoch ~p, loss: ~p, average: ~p~n", [Epoch, Rss, Rss / Examples])
+    end, lists:seq(0, Epochs - 1)),
+  Examples * Epochs * 2.
+
+learn_until(InputLayer, OutputLayer, BiasNeurons, Token, RssEps, LearningRate, TrainingSet) ->
+  Examples = length(TrainingSet),
+  {_, Epochs} = until(
+    fun({X, _}) -> X < RssEps end,
+    fun({_, Epoch}) ->
+      learn_pass(InputLayer, OutputLayer, BiasNeurons, Token + Examples * Epoch, LearningRate, TrainingSet, Examples),
+      Rss = forward_pass_examples(InputLayer, OutputLayer, BiasNeurons, Token + Examples * (Epoch + 1), TrainingSet, Examples),
+      io:format("Epoch ~p, loss: ~p, average: ~p~n", [Epoch div 2, Rss, Rss / Examples]),
+      {Rss, Epoch + 2}
+    end, {RssEps * 2, 0}),
+  Examples * Epochs.
+
+learn_pass(InputLayer, OutputLayer, BiasNeurons, Token, LearningRate, TrainingSet, Examples) ->
+  Self = self(),
+  lists:foreach(fun({Shift, {Input, Output}}) ->
+                  TokenShift = Token + Shift,
+                  lists:foreach(fun(N) -> 
+                                  N ! {back_prop, LearningRate, TokenShift} 
+                                end, BiasNeurons),
+                  lists:foreach(fun({N, In}) ->
+                                  N ! {back_prop, LearningRate, TokenShift},
+                                  N ! {fire, Self, In, TokenShift}
+                                end, lists:zip(InputLayer, Input)),
+                  lists:foreach(fun({N, Out}) ->
+                                  N ! {target, Out, TokenShift}
+                                end, lists:zip(OutputLayer, Output)),
+                  [receive {output, Pid, Out, TokenShift} -> Out end || Pid <- OutputLayer]
+                end, lists:zip(lists:seq(0, Examples - 1), TrainingSet)).
+
+forward_pass_examples(InputLayer, OutputLayer, BiasNeurons, Token, TrainingSet, Examples) ->
+  Rss = lists:map(fun({Shift, {ExIn, ExOut}}) ->
+                    TokenShift = Token + Shift,
+                    Output = forward_pass(InputLayer, OutputLayer, BiasNeurons, TokenShift, ExIn),
+                    compute_rss(Output, ExOut)
+                  end, lists:zip(lists:seq(0, Examples - 1), TrainingSet)),
+  lists:sum(Rss).
+
+forward_pass(InputLayer, OutputLayer, BiasNeurons, Token, Example) ->
+  Self = self(),
+  lists:foreach(fun(Pid) -> 
+                  Pid ! {feed_forward, Token} 
+                end, BiasNeurons),
+  lists:foreach(fun({Pid, In}) -> 
+                  Pid ! {feed_forward, Token}, 
+                  Pid ! {fire, Self, In, Token} 
+                end, lists:zip(InputLayer, Example)),
+  [receive {output, Pid, Out, Token} -> Out end || Pid <- OutputLayer].
+
+compute_rss(Xs, Ys) -> compute_rss(Xs, Ys, 0.0).
+compute_rss([], [], R) -> R;
+compute_rss([X | Xs], [Y | Ys], R) -> 
+  D = X - Y,
+  compute_rss(Xs, Ys, R + D * D).
+
+connect(InPidWeight, OutPid) ->
+  OutPid ! {connect_to_input, InPidWeight},
+  {InPid, _} = InPidWeight,
+  InPid ! {connect_to_output, OutPid},
+  io:format("connected ~p to ~p ~n", [InPid, OutPid]).
+
+full_mesh_connect(_, []) -> ok;
+full_mesh_connect([N1, N2], W) -> 
+  full_mesh_connect_layers(N1, N2, W);
+full_mesh_connect([N1, N2 | Ns], W) ->
+  Ws = full_mesh_connect_layers(N1, tl(N2), W),
+  full_mesh_connect([N2 | Ns], Ws).
+
+full_mesh_connect_layers(_, [], W) -> W;
+full_mesh_connect_layers(N1, [N | Ns], W) ->
+  Ws = full_mesh_connect_layer(N1, N, W),
+  full_mesh_connect_layers(N1, Ns, Ws).
+
+full_mesh_connect_layer([], _, W) -> W;
+full_mesh_connect_layer([N1 | Ns], N, [W | Ws]) ->
+  connect({N1, W}, N),
+  full_mesh_connect_layer(Ns, N, Ws).
+
+
+%% -----
+% utility functions
+%% -----
+sum_tuples(L) -> sum_tuples(L, 0).
+sum_tuples([], Acc) -> Acc;
+sum_tuples([{X, _} | Xs], Acc) -> sum_tuples(Xs, Acc + X).
+
+snd([]) -> [];
+snd([{_, X} | Xs]) -> [X | snd(Xs)].
+
+modify_layers([]) -> [];
+modify_layers([L]) -> [L];
+modify_layers([L | Ls]) -> [L + 1 | modify_layers(Ls)].
+
+compute_neurons([]) -> 0;
+compute_neurons([L]) -> L;
+compute_neurons([L1, L2 | Ls]) -> (L1 + 1) * L2 + compute_neurons([L2 | Ls]).
+
+random_weigths(N) -> [rand:uniform() * 2.0 - 1.0 || _ <- lists:seq(1, N)].
+
+random_shuffle(L) ->
+  [X || {_, X} <- lists:sort([{rand:uniform(), N} || N <- L])].
+
+until(P, F, X) ->
+  case P(X) of
+    false -> until(P, F, F(X));
+    true -> X
+  end.
+
+%% -----
+% read/write functions
+%% -----
+read_train_set(FileName, NumOfFeatures) ->
+  Lines = read_lines(FileName),
+  Numbers = lists:map(fun(L) -> lists:map(fun(X) -> parse_float(X) end, string:tokens(L, " ")) end, Lines),
+  lists:map(fun(L) -> lists:split(NumOfFeatures, L) end, Numbers).
+
+write_data(FileName, Data) ->
+  Out = string:join([string:join([io_lib:format("~p", [X]) || X <- Y], " ") || Y <- Data], "\n"),
+  file:write_file(FileName, io_lib:fwrite(Out, [])).
+
+read_lines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    try get_lines(Device)
+      after file:close(Device)
+    end.
+
+get_lines(Device) ->
+    case io:get_line(Device, "") of
+        eof  -> [];
+        Line -> [Line | get_lines(Device)]
+    end.
+
+parse_float(String) ->
+  T = string:strip(String, both, $\n),
+  S = string:strip(T, both),
+  case string:to_float(S) of
+    {error, no_float} -> float(list_to_integer(S));
+    {F, _} -> F
+  end.
---
+++ src/egambo_tictac_ann.erl
@@ -0,0 +1,258 @@
+-module(egambo_tictac_ann).
+
+-include("egambo_tictac.hrl").  % import tictac game definitions 
+
+-behavior(gen_server).          % callbacks provided by (bot) players
+%-behavior(egambo_gen_player).   % callbacks provided by (bot) players
+
+-record(state,  { bid        :: egBotId()       % bot id (= module name)
+                , tid        :: egGameTypeId()  % game type id
+                , engine     :: egEngine()      % game engine (rule engine)
+                , width=3    :: integer()       % board width >= 3
+                , height=3   :: integer()       % board height >= 3
+                , run=3      :: integer()       % sucess run length
+                , gravity=false :: boolean()    % do moves fall towards higher row numbers
+                , periodic=false :: boolean()   % unbounded repeating board
+                , winmod     :: egWinId()       % win function module
+                , players=2  :: integer()       % number of players 
+                , status     :: egBotStatus()   % current bot status (e.g. learning / playing)
+                , layers=[]  :: [integer()]     % neuron count per  layer
+                , rate=0.1   :: float()         % default learning rate
+                , epochs=10  :: integer()       % default learning epochs
+                , error=0.001 :: float()        % default learning error
+                , network  :: undefined | pid() % network root process
+                , gid        :: egGameId()      % game id of current move
+                , board= <<>> :: binary()       % current board (game state) before next move
+                }).
+
+% gen_server behavior callback exports
+
+-export([ start_link/1
+        , init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
+        ]).
+
+% egambo_gen_engine behavior callbacks, used by eg_game (game manager)
+-export([ resume/1      % instruct supervisor to start the bot process and resume learning/playing
+        , stop/1        % instruct supervisor to stop the bot process (no attempt to save the state)
+        , game_types/1  % return list of supported game types for given game engine (or atom 'all') 
+        ]).
+
+% learning and debugging API
+-export([ state/1
+        , start_learning/1
+        , start_playing/1
+        , learn_epochs/4
+        , learn_until/4
+        , save/1
+        ]).
+
+-define(BOT_IS_BUSY, {error, bot_is_busy}).
+
+game_types(egambo_tictac) -> all;
+game_types(_) -> [].
+
+-spec resume(egGameTypeId()) -> ok | egGameError().
+resume(GameTypeId) ->
+    ChildSpec = { ?BOT_ID(?MODULE, GameTypeId)                  % ChildId
+                , {?MODULE, start_link, [GameTypeId]}           % {M,F,A}
+                , permanent                                     % do restart automatically
+                , 1000                                          % Shutdown timeout
+                , worker                                        % Type
+                , [?MODULE]                                     % Modules
+                },
+    case supervisor:start_child(egambo_sup, ChildSpec) of
+        {ok,_} ->                       ok;
+        {ok,_,_} ->                     ok;
+        {error, already_present} ->     ok;
+        {error,{already_started,_}} ->  ok;
+        Error ->                        Error
+    end.
+
+learn_epochs(GameTypeId, Epochs, Rate, TrainingView) ->
+    gen_server:cast(?BOT_GID(?MODULE, GameTypeId), {learn_epochs, Epochs, Rate, TrainingView}).
+
+learn_until(GameTypeId, Tolerance, Rate, TrainingView) ->
+    gen_server:cast(?BOT_GID(?MODULE, GameTypeId), {learn_until, Tolerance, Rate, TrainingView}).
+
+-spec stop(egBotId()) -> ok | egGameError().
+stop(GameTypeId) ->
+    gen_server:call(?BOT_GID(?MODULE, GameTypeId), finish), 
+    supervisor:terminate_child(egambo_sup, ?BOT_ID(?MODULE, GameTypeId)),
+    supervisor:delete_child(egambo_sup, ?BOT_ID(?MODULE, GameTypeId)).
+
+state(GameTypeId) ->
+    gen_server:call(?BOT_GID(?MODULE, GameTypeId), state). 
+
+save(GameTypeId) ->
+    gen_server:call(?BOT_GID(?MODULE, GameTypeId), save). 
+
+start_learning(GameTypeId) ->
+    gen_server:call(?BOT_GID(?MODULE, GameTypeId), start_learning). 
+
+start_playing(GameTypeId) ->
+    gen_server:call(?BOT_GID(?MODULE, GameTypeId), start_playing). 
+
+init([GameTypeId]) ->
+    Result = try
+        #egGameType{engine=Engine, players=Players, params=Params} = egambo_game:read_type(GameTypeId),
+        Width=maps:get(width, Params),
+        Height=maps:get(height, Params),
+        Run=maps:get(run, Params),
+        Gravity=maps:get(gravity, Params),
+        Periodic=maps:get(periodic, Params),
+        Layers=[Width*Height+1|[Width*Height || _ <- lists:seq(2, Width)]],
+        Network=ann:create_neural_network(Layers),
+        State = #state{ bid=?MODULE 
+                      , tid=GameTypeId
+                      , engine=Engine
+                      , width=Width
+                      , height=Height
+                      , run=Run
+                      , gravity=Gravity
+                      , periodic=Periodic
+                      , winmod=egambo_tictac:win_module(Width, Height, Run, Periodic)
+                      , players=Players
+                      , status=learning
+                      , layers=Layers
+                      , network=Network
+                      },
+        process_flag(trap_exit, true),
+        {ok, State}
+    catch
+        _Class:Reason -> {stop, {Reason,erlang:get_stacktrace()}} 
+    end,
+    Result.
+
+start_link(GameTypeId)  ->
+    gen_server:start_link(?BOT_GID(?MODULE, GameTypeId), ?MODULE, [GameTypeId], []).
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+handle_cast({play_bot_req, GameId, _, [Player|_]}, #state{status=Status} = State) when Status /= playing ->
+    play_bot_resp(GameId, Player, ?BOT_NOT_PLAYING),
+    {noreply, State};
+handle_cast({play_bot_req, GameId, _, [Player|_]}, #state{gid=GameId} = State) when GameId /= undefined ->
+    play_bot_resp(GameId, Player, ?BOT_IS_BUSY),
+    {noreply, State};
+handle_cast({play_bot_req, GameId, Board, Aliases}, #state{ width=Width, height=Height, run=Run, network=Network
+                                                          , gravity=Gravity, periodic=Periodic, winmod=WinMod
+                                                          } = State) -> 
+    Options = put_options(Board, Width, Gravity),
+    case play_bot_immediate_win(Board, Width, Height, Run, Gravity, Periodic, WinMod, Aliases, Options) of
+        {ok, Idx, NewBoard} ->              % win in this move detected
+            play_bot_resp(GameId, hd(Aliases), {ok, Idx, NewBoard}),
+            {noreply, State}; 
+        {nok, no_immediate_win} ->
+            case play_bot_defend_immediate(Board, Width, Height, Run, Gravity, Periodic, WinMod, Aliases, Options) of
+                {ok, Idx, NewBoard} ->      % opponent's win in this move detected and taken
+                    play_bot_resp(GameId, hd(Aliases), {ok, Idx, NewBoard}),
+                    {noreply, State};
+                {nok, no_immediate_risk} ->
+                    play_bot_ann(Board, Aliases, Network),
+                    {noreply, State#state{gid=GameId, board=Board, players=Aliases}};
+                Error -> 
+                    play_bot_resp(GameId, hd(Aliases), Error),
+                    {noreply, State}
+            end;
+        Error -> 
+            play_bot_resp(GameId, hd(Aliases), Error),
+            {noreply, State}
+    end;
+handle_cast({learn_until, Tolerance, Rate, TrainingSet}, #state{network=Network, status=learning} = State) ->
+    Network ! {learn_until, Tolerance, Rate, TrainingSet},
+    {noreply, State};
+handle_cast({learn_epochs, Epochs, Rate, TrainingSet}, #state{network=Network, status=learning} = State) ->
+    Network ! {learn_epochs, Epochs, Rate, TrainingSet},
+    {noreply, State};
+handle_cast(Request, State) -> 
+    ?Info("Unsolicited handle_cast in ~p : ~p",[?MODULE, Request]),
+    {noreply, State}.
+
+handle_info({predicted, Output}, #state{gid=GameId, board=Board, width=Width, gravity=Gravity, players=[Player|_] } = State) ->
+    BotMove = pick_output(Output, Board, Width, Gravity, Player),
+    play_bot_resp(GameId, Player, BotMove),
+    {noreply, State#state{gid=undefined}};    
+handle_info(Request, State) -> 
+    ?Info("Unsolicited handle_info in ~p : ~p",[?MODULE, Request]),
+    {noreply, State}.
+
+handle_call(finish, _From, #state{network=Network} = State) ->
+    Network ! finish,
+    {reply, ok, State#state{network=undefined}};
+handle_call(start_learning, _From, State) ->
+    {reply, ok, State#state{status=learning}};
+handle_call(start_playing, _From, State) ->
+    {reply, ok, State#state{status=playing}};
+handle_call(save, _From, State) ->
+    {reply, ?egGameNotImplemented, State};
+handle_call(state, _From, State) ->
+    {reply, State, State};
+handle_call(stop, _From, State) ->
+    {stop, normal, ok, State}.
+
+pick_output(Output, Board, Width, Gravity, Player) ->
+    SortedOutput = lists:sort([{Gain,I} || {Gain,I} <- lists:zip(Output, lists:seq(0, length(Output)-1))]),   
+    pick_output_sorted(SortedOutput, Board, Width, Gravity, Player).
+
+pick_output_sorted([], Board, Width, Gravity, Player) ->
+    ?Info("Random move played by ~p in ~p for board ~p",[Player, ?MODULE, Board]),
+    play_bot_random(Board, Width, Gravity, Player);
+pick_output_sorted([{_,I}|Outputs], Board, Width, Gravity, Player) ->
+    case egambo_tictac:put(Gravity, Board, Width, I, Player) of
+        {ok, Idx, NewBoard} ->  {ok, Idx, NewBoard};
+        _ ->                    pick_output_sorted(Outputs, Board, Width, Gravity, Player)
+    end.
+
+-spec play_bot_resp(egGameId(), egAlias(), egBotMove() ) -> ok.
+play_bot_resp(GameId, Player, BotMove) -> 
+    send_to_engine(GameId, {play_bot_resp, Player, BotMove}).
+
+-spec send_to_engine(egGameId(), egBotMoveCmd()) -> ok.
+send_to_engine(GameId, Message) ->
+    try 
+        global:send(?ENGINE_ID(GameId), Message),
+        ok
+    catch 
+        exit:{badarg, {_, _}} -> ?Error("GameEngine ~p unreachable. Dropped message: ~p",[GameId, Message])
+    end.
+
+random_idx1(Length) -> crypto:rand_uniform(1, Length+1). % 1..Length / 1 based random integer
+
+-spec play_bot_random(binary(), integer(), boolean(), egAlias()) -> {ok, Move::integer(), NewBoard::binary()} | {error, atom()}.
+play_bot_random(Board, Width, Gravity, Player) ->
+    Options = put_options(Board, Width, Gravity),
+    egambo_tictac:put(Gravity, Board, Width, lists:nth(random_idx1(length(Options)), Options), Player).
+
+-spec play_bot_ann(binary(), [egAlias()], pid()) -> ok | egGameError().
+play_bot_ann(Board, [Player|_], Network) ->
+    Network ! {predict, [Player|binary_to_list(Board)], self()}.
+
+-spec play_bot_immediate_win(binary(), integer(), integer(), integer(), boolean(), boolean(), egWinId(), [egAlias()], Options::[egGameMove()]) -> {ok, integer(), binary()} | {nok, no_immediate_win} | {error, atom()}.
+play_bot_immediate_win(_Board, _Width, _Height, _Run, _Gravity, _Periodic, _WinMod, _Aliases, []) -> {nok, no_immediate_win};  
+play_bot_immediate_win(Board, Width, Height, Run, Gravity, Periodic, WinMod, Aliases, [I|Rest]) -> 
+    {ok, Idx, TestBoard} = egambo_tictac:put(Gravity, Board, Width, I, hd(Aliases)),
+    case WinMod:win(TestBoard, Aliases) of
+        true ->     {ok, Idx, TestBoard};
+        false ->    play_bot_immediate_win(Board, Width, Height, Run, Gravity, Periodic, WinMod, Aliases, Rest)
+    end.
+
+-spec play_bot_defend_immediate(binary(), integer(), integer(), integer(), boolean(), boolean(), egWinId(), [egAlias()], Options::[egGameMove()]) -> {ok, Move::integer(), NewBoard::binary()} | {nok, no_immediate_risk} | {error, atom()}.
+play_bot_defend_immediate(_Board, _Width, _Height, _Run, _Gravity, _Periodic, _WinMod, _Aliases, []) -> {nok, no_immediate_risk};
+play_bot_defend_immediate(Board, Width, Height, Run, Gravity, Periodic, WinMod, [Player|Others], [I|Rest]) -> 
+    {ok, Idx, TestBoard} = egambo_tictac:put(Gravity, Board, Width, I, hd(Others)),
+    case WinMod:win(TestBoard, Others) of
+        true ->     egambo_tictac:put(Gravity, Board, Width, Idx, Player);
+        false ->    play_bot_defend_immediate(Board, Width, Height, Run, Gravity, Periodic, WinMod, [Player|Others], Rest)
+    end.
+
+put_options(Board, _Width, false) ->
+    lists:usort([ case B of ?AVAILABLE -> I; _ -> false end || {B,I} <- lists:zip(binary_to_list(Board), lists:seq(0,size(Board)-1))]) -- [false];
+put_options(Board, Width, true) ->
+    lists:usort([ case B of ?AVAILABLE -> I; _ -> false end || {B,I} <- lists:zip(binary_to_list(binary:part(Board,0,Width)), lists:seq(0,Width-1))]) -- [false].
