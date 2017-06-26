3dd2b31f13269531f819c6cbd6cb5f6ab23f5d35
Agustin Cautin <acautin@gmail.com>
2017-06-26 14:46:22 +0200

    Added base empty components (boards and moves) for the angular GUI and fixed some file format.

---
+++ priv/dev/src/app/board/board.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-board',
+    templateUrl: './board.component.html',
+    styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
